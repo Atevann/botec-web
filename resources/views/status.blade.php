030aea737ee60fa8d6e7c9f08974d8b4a6410b8a
Amirkhan <amirhan75@gmail.com>
2024-02-25 23:09:42 +0600

    BCW-00007 Добавил дропдаун языка

--- resources/views/status.blade.php
+++ resources/views/status.blade.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>botec-web</title>
-</head>
-<body>
+<x-app-layout>
     <h1>Status botec-web</h1>
     @foreach ($statusArray as $status)
     <p>{{$status->getname()}}:
@@ -17,5 +10,4 @@
 
     </p>
     @endforeach
-</body>
-</html>
+</x-app-layout>
